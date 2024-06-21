6af34aa437d5661319c8f73f0a0385d31eb1edb2
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2024-06-21 17:41:05 +0900

    Update ReleaseAll.sh

--- ReleaseAll.sh
+++ ReleaseAll.sh
@@ -1,10 +1,10 @@
 BASEDIR=$(dirname "$0")
 
-pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/OpenBiddingHelper-Dev.podspec --allow-warnings --skip-import-validation
-pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadAdFitAdapter-Dev.podspec --allow-warnings --skip-import-validation
-pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadAdPopcornAdapter-Dev.podspec --allow-warnings --skip-import-validation
-pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadAllianceInternetAdapter-Dev.podspec --allow-warnings --skip-import-validation
-pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadGoogleAdManagerAdapter-Dev.podspec --allow-warnings --skip-import-validation
-pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadGoogleAdMobAdapter-Dev.podspec --allow-warnings --skip-import-validation
-pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadUnityAdsAdapter-Dev.podspec --allow-warnings --skip-import-validation
-pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadMobonAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push $BASEDIR/OpenBiddingHelper-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push $BASEDIR/BidmadAdFitAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push $BASEDIR/BidmadAdPopcornAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push $BASEDIR/BidmadAllianceInternetAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push $BASEDIR/BidmadGoogleAdManagerAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push $BASEDIR/BidmadGoogleAdMobAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push $BASEDIR/BidmadUnityAdsAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push $BASEDIR/BidmadMobonAdapter-Dev.podspec --allow-warnings --skip-import-validation

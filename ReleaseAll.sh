f01b8193c8ca35f8972ffc1320501008fd496084
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2024-06-21 17:40:21 +0900

    Update ReleaseAll.sh

--- ReleaseAll.sh
+++ ReleaseAll.sh
@@ -1,12 +1,10 @@
 BASEDIR=$(dirname "$0")
 
-pod repo push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadSDK-Dev.podspec --allow-warnings --skip-import-validation
-pod repo push BidmadSDK-CocoaPods-Specs $BASEDIR/OpenBiddingHelper-Dev.podspec --allow-warnings --skip-import-validation
-pod repo push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadAdFitAdapter-Dev.podspec --allow-warnings --skip-import-validation
-pod repo push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadAdPopcornAdapter-Dev.podspec --allow-warnings --skip-import-validation
-pod repo push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadAllianceInternetAdapter-Dev.podspec --allow-warnings --skip-import-validation
-pod repo push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadGoogleAdManagerAdapter-Dev.podspec --allow-warnings --skip-import-validation
-pod repo push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadGoogleAdMobAdapter-Dev.podspec --allow-warnings --skip-import-validation
-pod repo push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadUnityAdsAdapter-Dev.podspec --allow-warnings --skip-import-validation
-pod repo push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadMobonAdapter-Dev.podspec --allow-warnings --skip-import-validation
-pod repo push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadFlutterBridge-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/OpenBiddingHelper-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadAdFitAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadAdPopcornAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadAllianceInternetAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadGoogleAdManagerAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadGoogleAdMobAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadUnityAdsAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push BidmadSDK-CocoaPods-Specs $BASEDIR/BidmadMobonAdapter-Dev.podspec --allow-warnings --skip-import-validation

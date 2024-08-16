0e1936f915db3913f01eb1b4dab37b4c8e30af6c
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2024-08-16 16:36:47 +0900

    Update

--- BidmadAdPopcornAdapter-Dev.podspec
+++ BidmadAdPopcornAdapter-Dev.podspec
@@ -1,20 +1,20 @@
 Pod::Spec.new do |s|
 
   s.name             = "BidmadAdPopcornAdapter-Dev"
-  s.version          = "2.6.5.8.0"
+  s.version          = "2.6.5.8.1"
   s.platform         = :ios, "12.0"
   s.summary          = "Integrated App Monetization Solution for Mobile Apps by all Publishers"
   s.description      = "BidmadSDK Mediation Adapter for Ad Popcorn"
   s.homepage         = "https://bidmad.net"
   s.license          = { :type => "MIT", :file => "LICENSE" }
   s.author           = { "Markus" => "markus@adop.cc" }
-  s.source           = { :git => 'https://github.com/adop-devel/BidmadSDK-CocoaPods.git', :tag => 'BidmadAdPopcornAdapter-Dev.2.6.5.8.0' }
+  s.source           = { :git => 'https://github.com/adop-devel/BidmadSDK-CocoaPods.git', :tag => 'BidmadAdPopcornAdapter-Dev.2.6.5.8.1' }
   s.static_framework = true
   s.requires_arc     = true
   s.vendored_frameworks = "BidmadAdPopcornAdapter.xcframework"
   s.swift_version = '5.0'
 
-  s.dependency 'BidmadSDK-Dev', '>=6.9.0.0', '<6.10.0'
+  s.dependency 'BidmadSDK-Dev', '>=6.9.0.1', '<6.10.0'
   s.dependency 'AdPopcornSSP', '2.6.5'
   s.dependency 'NAMSDK', '7.5.3'
   s.dependency 'NAMSDK/MediationNDA', '7.5.3'
--- ReleaseAll.sh
+++ ReleaseAll.sh
@@ -1,10 +1,12 @@
 BASEDIR=$(dirname "$0")
 
-pod trunk push $BASEDIR/OpenBiddingHelper-Dev.podspec --allow-warnings --skip-import-validation
 pod trunk push $BASEDIR/BidmadAdFitAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push $BASEDIR/BidmadAdpieAdapter-Dev.podspec --allow-warnings --skip-import-validation
 pod trunk push $BASEDIR/BidmadAdPopcornAdapter-Dev.podspec --allow-warnings --skip-import-validation
 pod trunk push $BASEDIR/BidmadAllianceInternetAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push $BASEDIR/BidmadCoviAdapter-Dev.podspec --allow-warnings --skip-import-validation
 pod trunk push $BASEDIR/BidmadGoogleAdManagerAdapter-Dev.podspec --allow-warnings --skip-import-validation
 pod trunk push $BASEDIR/BidmadGoogleAdMobAdapter-Dev.podspec --allow-warnings --skip-import-validation
-pod trunk push $BASEDIR/BidmadUnityAdsAdapter-Dev.podspec --allow-warnings --skip-import-validation
 pod trunk push $BASEDIR/BidmadMobonAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push $BASEDIR/BidmadMobwithAdapter-Dev.podspec --allow-warnings --skip-import-validation
+pod trunk push $BASEDIR/BidmadUnityAdsAdapter-Dev.podspec --allow-warnings --skip-import-validation
